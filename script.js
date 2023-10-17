172c1eb745abee648d824268b1cb55b50333c5d7
concord01 <113149352+concord01@users.noreply.github.com>
2023-10-17 20:51:41 +0000

    adding data to script

--- script.js
+++ script.js
@@ -1,8 +1,101 @@
+let stolenItems = [
+  {
+   socks: 7,
+   spoons: 20,
+   nutella_jar: 5,
+   keys: 0,
+   shoelaces: 4,
+   rotisserie_chicken:1
+ },
+ 
+ {
+   socks: 2,
+   spoons: 2,
+   nutella_jar: 0,
+   keys: 0,
+   shoelaces: 4, 
+   rotisserie_chicken: 2
+ },
+   
+ {
+   socks: 0,
+   spoons: 0,
+   nutella_jar: 0,
+   keys: 0,
+   shoelaces: 0,
+   rotisserie_chicken: 0
+ },
+ 
+   {
+   socks: 2,
+   spoons: 10,
+   nutella_jar: 4,
+   keys: 4,
+   shoelaces: 12,
+   rotisserie_chicken: 0
+ }, 
+   
+ {
+   socks: 12,
+   spoons: 17,
+   nutella_jar: 0,
+   keys: 2,
+   shoelaces: 0,
+   rotisserie_chicken: 0
+ },
+ 
+ {
+   socks: 0,
+   spoons: 0,
+   nutella_jar: 2,
+   keys: 0,
+   shoelaces: 0,
+   rotisserie_chicken: 0
+ }, 
+   
+ {
+   socks: 2,
+   spoons: 2,
+   nutella_jar: 0,
+   keys: 4,
+   shoelaces: 1,
+   rotisserie_chicken: 0
+ }, 
+   
+ {
+   socks: 43,
+   spoons: 10,
+   nutella_jar: 50,
+   keys: 2,
+   shoelaces: 0,
+   rotisserie_chicken: 0
+ }, 
+ 
+ {
+   socks: 0,
+   spoons: 2,
+   nutella_jar: 0,
+   keys: 3,
+   shoelaces: 0,
+   rotisserie_chicken: 0
+ }, 
+ {
+   socks: 0,
+   spoons: 0,
+   nutella_jar: 0,
+   keys: 0,
+   shoelaces: 14,
+   rotisserie_chicken: 0
+ }
+ 
+ ]
 // PROBLEM ONE
 // Given an object of the stolen items for a household, return the total amount of items stolen (number). If nothing was robbed, return the string "Lucky you!”.
 
 function houseTotal(obj) {
-
+for(i=0;i<obj.length;i++){
+  stolenItems[i]
+}
 
 return 
 
